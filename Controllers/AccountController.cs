058c3a0eb45281fabf5c4590a03f98bf73b6a210
Suraj6392 <Suraj6392verma@gmail.com>
2024-01-13 17:16:38 +0530

    Upload New Files

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BahanKiSadi_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost(Name = "Register")]
+        public IActionResult Register()
+        {
+            return null;
+        }
+    }
+}
